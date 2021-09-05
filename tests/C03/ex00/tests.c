9e6f535b234b688c70fafc275dd408c396933aa0
Lotho Teresia <lteresia@student.21-school.ru>
2021-09-05 20:27:17 +0300

    C03 full

--- tests/C03/ex00/tests.c
+++ tests/C03/ex00/tests.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+int	sgn(int result)
+{
+	if (result > 0)
+		result = 1;
+	else if (result < 0)
+		result = -1;
+	return (result);
+}
+
+void	test(char *s1, char *s2)
+{
+	printf("%d\n", sgn(strcmp(s1, s2)) == sgn(ft_strcmp(s1, s2)));
+}
 
 int	main(void)
 {
-	printf("%d\n", ft_strcmp("Equal strings", "Equal strings"));
-	printf("%d\n", ft_strcmp("Str1ng is bigger", "Str1ng is smaller") > 0 ? 1 : -1);
-	printf("%d\n", ft_strcmp("Str1ng is Bigger", "Str1ng is bigger") > 0 ? 1 : -1);
-	printf("%d\n", ft_strcmp("", ""));
-	printf("%d\n", ft_strcmp("Str1ng is bigger", "Str1ng is") > 0 ? 1 : -1);
-	printf("%d\n", ft_strcmp("Str1ng is smaller", "Str1ng is smaller?") > 0 ? 1 : -1);
+	test("Equal strings", "Equal strings");
+	test("Str1ng is bigger", "Str1ng is smaller");
+	test("Str1ng is Bigger", "Str1ng is bigger");
+	test("", "");
+	test("Str1ng is bigger", "Str1ng is");
+	test("Str1ng is smaller", "Str1ng is smaller?");
 }
--- tests/C03/ex01/tests.c
+++ tests/C03/ex01/tests.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-void	printf_result(int result)
+int	sgn(int result)
 {
 	if (result > 0)
 		result = 1;
 	else if (result < 0)
 		result = -1;
-	printf("%d\n", result);
+	return (result);
+}
+
+void	test(char *s1, char *s2, unsigned int n)
+{
+	printf("%d\n", sgn(strncmp(s1, s2, n)) == sgn(ft_strncmp(s1, s2, n)));
 }
 
 
 int	main(void)
 {
-	printf_result(ft_strncmp("Equal strings", "Equal strings", 5));
-	printf_result(ft_strncmp("Str1ng is Bigger", "Str1ng is bigger", 10));
-	printf_result(ft_strncmp("Str1ng is bigger", "Str1ng is biGger", 14));
-	printf_result(ft_strncmp("", "", 1));
-	printf_result(ft_strncmp("Str1ng is bigger", "Str1ng is", 100));
-	printf_result(ft_strncmp("Str1ng", "Str1ng is smaller?", 15));
-	printf_result(ft_strncmp("String", "Str1ng is smaller?", 15));
-	printf_result(ft_strncmp("Atring", "Str1ng?", 0));
-	printf_result(ft_strncmp("Str1ng is Bigger", "Str1ng is_bigger", 10));	
+	test("Equal strings", "Equal strings", 5);
+	test("Str1ng is Bigger", "Str1ng is bigger", 10);
+	test("Str1ng is bigger", "Str1ng is biGger", 14);
+	test("", "", 1);
+	test("Str1ng is bigger", "Str1ng is", 100);
+	test("Str1ng", "Str1ng is smaller?", 15);
+	test("String", "Str1ng is smaller?", 15);
+	test("Atring", "Str1ng?", 0);
+	test("Str1ng is Bigger", "Str1ng is_bigger", 10);
+	return (0);
 }
